dbdcd76ad42ffee719f8e455b854de75f3531b05
Exxa1 <akuna@ruc.dk>
2022-05-09 11:49:25 +0200

    Calibration - signal when hand is turned

--- All finger prototype apr1.py
+++ All finger prototype apr1.py
@@ -1,6 +1,9 @@
 from turtle import distance
+
+from cv2 import subtract
 import hand_track_class
 import math
+import numpy as np
 import socket
 import time
 
@@ -31,6 +34,22 @@ while True:
         thumb_distance = (math.dist(thumb_coord, ring_mcp_coord)/horizontal_distance - 0.6) / (2.8 - 0.6)
         distance_touple= [index_distance, middle_distance, ring_distance, pinky_distance, thumb_distance]
 
+        #wristList = a; ring_mcp_coord = b; index_mcp_coord = c; Surface of triangle: (b-a) X (c-a)
+        # surface = np.cross(np.subtract(ring_mcp_coord, wristList), np.subtract(index_mcp_coord, wristList))
+        # print(surface)
+
+        is_partly_closed = pinky_coord[1] > ring_mcp_coord[1] and ring_coord[1] > ring_mcp_coord[1]
+        is_fully_closed = is_partly_closed and middle_coord[1] > ring_mcp_coord[1] and index_coord[1] > ring_mcp_coord[1]
+        # print(is_closed)
+
+        if not is_partly_closed and 2.4 < (vertical_distance/horizontal_distance) < 3.1:
+            print("open - good to go")
+        elif is_partly_closed and 2.4 < (vertical_distance/horizontal_distance) < 3.1:
+            print("closed - good to go")
+        elif is_fully_closed:
+            print("fully closed - good to go")
+        else:
+            print("nope")
 
 
         for i in range(5):
@@ -40,19 +59,21 @@ while True:
             elif distance_touple[i] < 0:
                 distance_touple[i] = 0
             distance_touple[i] = int(distance_touple[i])
+
+        # print(vertical_distance/horizontal_distance)
         
 
-        # # SENDING UPD MESSAGE
+        # SENDING UPD MESSAGE
         MESSAGE = "{:0>3d}:{:0>3d}:{:0>3d}:{:0>3d}:{:0>3d}".format(distance_touple[0],distance_touple[1], distance_touple[2], distance_touple[3], distance_touple[4])
 
-    #     UDP_IP = "192.168.48.216"
-    #     UDP_PORT = 4210
+        UDP_IP = "192.168.69.216"
+        UDP_PORT = 4210
 
-    #    # print ("UDP target IP:", UDP_IP)
-    #    # print ("UDP target port:", UDP_PORT)
+       # print ("UDP target IP:", UDP_IP)
+       # print ("UDP target port:", UDP_PORT)
         print ("message:", MESSAGE)
 
-    #     sock = socket.socket(socket.AF_INET, # Internet
-    #                 socket.SOCK_DGRAM) # UDP
+        sock = socket.socket(socket.AF_INET, # Internet
+                    socket.SOCK_DGRAM) # UDP
 
-    #     sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+        sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
